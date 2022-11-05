fb0d8da511273de04e884ff9f24728d927fe756f
Rodionov Roman <z0w@ya.ru>
2022-11-05 14:22:47 +0400

    add schemas for bet context to mikroorm

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,14 +1,20 @@
 import { Options } from '@mikro-orm/core';
 import field from './game/database/schemas/field';
 import session from './game/database/schemas/session';
-import entity from './game/database/schemas/entity';
+import gameEntity from './game/database/schemas/entity';
 import game from './game/database/schemas/game';
 import player from './game/database/schemas/player';
+import bet from './bet/schemas/bet';
+import betEntity from './bet/schemas/entity';
+import status from './bet/schemas/status';
+
+const gameSchemas = [field, session, gameEntity, game, player];
+const betSchemas = [bet, betEntity, status];
 
 const options: Options = {
     type: 'sqlite',
     dbName: process.NODE_ENV === 'test' ? 'humsters.test.db' : 'humsters.db',
-    entities: [field, session, entity, game, player]
+    entities: [...gameSchemas, ...betSchemas]
 };
 
 export default options;
