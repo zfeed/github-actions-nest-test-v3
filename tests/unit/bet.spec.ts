af1b16a866518c46f7bee22b40238f8c52fdb95d
Rodionov Roman <z0w@ya.ru>
2022-11-05 13:47:47 +0400

    add unit tests fro bet

---
+++ tests/unit/bet.spec.ts
@@ -0,0 +1,44 @@
+import Bet from '../../src/bet/core/domain/bet/Bet';
+import Status from '../../src/bet/core/domain/bet/Status';
+
+describe('Bet', () => {
+    test('Bet is created', () => {
+        const bet = Bet.create('bet-id-1', 'game-id-1', 10, [
+            'player-id-1',
+            'player-id-2',
+            'player-id-3'
+        ]);
+
+        expect(bet.id).toBe('bet-id-1');
+        expect(bet.gameId).toBe('game-id-1');
+        expect(bet.getWinnerPlayerId()).toBe(null);
+        expect(bet.amount).toBe(10);
+        expect(bet.getStatus()).toEqual(Status.create(Status.code.ACTIVE));
+        expect(bet.playerIds).toEqual([
+            'player-id-1',
+            'player-id-2',
+            'player-id-3'
+        ]);
+    });
+
+    test('Bet is finished', () => {
+        const bet = Bet.create('bet-id-1', 'game-id-1', 10, [
+            'player-id-1',
+            'player-id-2',
+            'player-id-3'
+        ]);
+
+        bet.finishBet('player-id-2');
+
+        expect(bet.id).toBe('bet-id-1');
+        expect(bet.gameId).toBe('game-id-1');
+        expect(bet.getWinnerPlayerId()).toBe('player-id-2');
+        expect(bet.amount).toBe(10);
+        expect(bet.getStatus()).toEqual(Status.create(Status.code.FINISHED));
+        expect(bet.playerIds).toEqual([
+            'player-id-1',
+            'player-id-2',
+            'player-id-3'
+        ]);
+    });
+});
