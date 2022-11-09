e15e04922869b8516cff249b8b9080088e3ef829
Rodionov Roman <z0w@ya.ru>
2022-11-09 17:16:17 +0400

    code formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,34 @@
 module.exports = {
-  env: {
-    node: true,
-    es2022: true,
-  },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
+    env: {
+        node: true,
+        es2022: true
     },
-  },
-  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-    indent: ['error', 4],
-    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        ts: 'never',
-      },
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx']
+            }
+        }
+    },
+    extends: [
+        'airbnb-base',
+        'plugin:@typescript-eslint/recommended',
+        'prettier'
     ],
-  },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    rules: {
+        indent: ['error', 4],
+        'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                ts: 'never'
+            }
+        ]
+    }
 };
